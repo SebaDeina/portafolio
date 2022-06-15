43110980d55e95a20a740871fcbe9a8639609f56
Sebastian Deina <sebadeina1@gmail.com>
2022-06-14 21:28:24 -0300

    Updates

---
+++ precache-manifest.c4912f67cbb3b2db39c9ec453b4034cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06f5f8a9d53bb1017a3f4d2586b4da6",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "e18e8de2739c7f78eb89",
+    "url": "/portafolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "2790bc6af21ba3643054",
+    "url": "/portafolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "e18e8de2739c7f78eb89",
+    "url": "/portafolio/static/js/2.17daa532.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portafolio/static/js/2.17daa532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2790bc6af21ba3643054",
+    "url": "/portafolio/static/js/main.c2da210c.chunk.js"
+  },
+  {
+    "revision": "4eb346c4eb6a999e4d30",
+    "url": "/portafolio/static/js/runtime-main.66972236.js"
+  }
+]);
